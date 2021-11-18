ddea407e982e1dfb496a9aaf8a1ab6f4797fc107
andychicken <83952965+andychicken@users.noreply.github.com>
2021-11-18 15:03:17 +0700

    sc primitives

---
+++ contracts/primitives.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+contract Primitives{
+    bool public boo = true;
+    uint public uint1 = 123;
+    address public addr;
+    int public int1 = -230;
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 (async () => {
-  const Greet = await ethers.getContractFactory('Hello');
+  const Greet = await ethers.getContractFactory('Primitives');
   const greet = await Greet.deploy();
   console.log(`Contract deploy at : ${greet.address}`);
 })();
