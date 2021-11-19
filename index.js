4f2633be67e2181ea94011e41961f1638cb962d0
andychicken <83952965+andychicken@users.noreply.github.com>
2021-11-19 17:08:00 +0700

    final

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('@nomiclabs/hardhat-ethers');
 
 (async () => {
-  const Greet = await ethers.getContractFactory('Primitives');
+  const Greet = await ethers.getContractFactory('SimpleStorage');
   const greet = await Greet.deploy();
   console.log(`Contract deploy at : ${greet.address}`);
 })();
+//npx hardhat run index.js
